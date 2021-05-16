f229885dd12c47bd9cdb6f982928e191810c90c8
Hieu Vu <hieu.vu@trustingsocial.com>
2021-05-17 02:47:37 +0700

    update

--- data_v2/datasets/dataset.py
+++ data_v2/datasets/dataset.py
@@ -251,6 +251,7 @@ class ImageDataset(Dataset):
         mask = np.fromfile(mask_path, dtype=int)
         if self.transform is not None:
             img = self.transform(img)
+        print("hieu: img.shape:", img.shape)
         # print("hieu: mask: ", mask.shape)
         return img, pid, camid, img_path, np.arange(1, 2)
         # return img, pid
